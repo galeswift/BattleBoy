8eecfc38866bb40af090e6e963bcb8cb25d649bc
jplou <johnplou@gmail.com>
2012-07-15 22:01:58 -0700

    Added BattleBoy project

---
+++ BattleBoy/main.cpp
@@ -0,0 +1,27 @@
+#include "Boy/Environment.h"
+#include "BattleBoy.h"
+
+int main(int argc, char* argv[])
+{
+	// initialize the platform:
+	Boy::Environment::instance()->init(
+		BattleBoy::instance(),
+		1024, // width
+		768, // height
+		false, // fullscreen
+		"demo1", // window title
+		"demo1.dat", // persistence layer file name
+		NULL); // no encryption
+
+	// start the main loop
+	Boy::Environment::instance()->startMainLoop();
+
+	// destroy the game:
+	BattleBoy::destroy();
+
+	// destroy the environment:
+	Boy::Environment::instance()->destroy();
+
+	return 0;
+}
+
